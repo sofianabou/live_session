bc9f3e37b33ee965579191fd2ded1cf80f2809b7
sofianabou <yahia.sofiane04@gmail.com>
2024-02-29 08:41:33 +0100

    fisrt commit react

---
+++ src/AfpaComponent.js
@@ -0,0 +1,10 @@
+export function AfpaComponent()
+{
+  return(
+    <div>
+          <h1>Pompey Academie</h1>
+          <h3>The Best Practice Programme</h3>
+
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { AfpaComponent } from './AfpaComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          
+        </p>
+        <h1>afpa learning React</h1>
+        <AfpaComponent/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
